d528d1844395561b105d56a074d77648967e41e9
bestwestern <runjep@gmail.com>
2023-11-10 19:12:45 +0100

    hurray

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,25 @@ import { h, Fragment } from 'preact';
 import './Adminbutton.css';
 export default function Home({ showingAdmin,words,currentWord }) {
     const click=()=>{
-        console.log("click")
+        
         const currentId=currentWord.value?currentWord.value.id:0;
+        // if (currentId){
+        //     let changedWord=words.value.find(el=>el.id===CurrentId);
+        //     const showcount=changedWord.showcount;
+            // supabase
+            // .from('ord')
+            // .update({ showcount:showcount+1 })
+            // .eq('id', currentId)
+            // .select().then(({data,error})=>{
+            //     console.log(data,error)
+            //     if (error) alert("noget gik galt");
+            // // let newWords=[...words.value];
+          
+            // // changedWord.archived=newArchiveValue;
+            // // words.value=newWords
+            // })
+
+        // }
         const possibleWords=words.value.filter(x=>!x.archived);
         let newWord= possibleWords[Math.floor(Math.random()*possibleWords.length)];
         while (newWord.id==currentId){
